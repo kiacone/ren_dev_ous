ea898f995ecc8f4b1e6b6dd26769bd750fe039c6
Topher <tophersymps@gmail.com>
2018-07-18 23:58:19 -0400

    missing files

---
+++ client/src/assets/jss/material-dashboard-react/layouts/home.jsx
@@ -0,0 +1,19 @@
+import {
+  drawerWidth,
+  transition,
+  container
+} from "../../material-dashboard-react.jsx";
+import { Backdrop } from "../../../../../../node_modules/@material-ui/core";
+
+const appStyle = theme => ({
+  container: {
+    // marginTop: "70px"
+    padding: "50px 0px"
+    
+  },
+  introSpace:{
+    padding: "50px 90px" 
+  }
+});
+
+export default appStyle;
